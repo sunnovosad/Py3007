3a77c24e01329e493f1c10560c52ba2e52870f13
Novosad Vitaliy <sunnovosad@gmail.com>
2018-08-13 12:54:51 +0300

    my first initial commit for Py3007 repo

---
+++ Lesson_01/01_home_task_1.py
@@ -0,0 +1,19 @@
+# task_1
+# Запрограммировать следующее выражение: (a + b - f / a) + f * a * a - (a + b)
+# Числа а, b, f вводятся с клавиатуры. ...
+
+a = int(input('Введите A: '))
+b = int(input('Введите B: '))
+f = int(input('Введите F: '))
+
+result = a + b - f / a + f * a * a - (a + b)
+#result1 = a + b - f / a + f * a ** 2 - (a + b)
+print ('Результат:', result)
+#print ('Результат:', result1)
+
+
+
+
+
+
+
