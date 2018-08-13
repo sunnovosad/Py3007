647076e8e9c5d186c1b4734dcfeab49875589911
Novosad Vitaliy <sunnovosad@gmail.com>
2018-08-13 15:16:43 +0300

    01_home_task2 completed

--- Lesson_01/01_home_task_2.py
+++ Lesson_01/01_home_task_2.py
@@ -1,6 +1,25 @@
-# 2. Вычислить возраст человека. Программа принимает с клавиатуры год рождения и выводит возраст на экран.
+# 2. Вычислить возраст человека. Программа принимает с клавиатуры год рождения 
+# и выводит возраст на экран.
 # Пример:
 # Ваша дата рождения:
 # 1990
 # Вам 28 лет
 #
+
+import datetime
+#examples
+#print("Current date and time: ", datetime.datetime.now())
+#a = datetime.datetime.today().year
+#b = datetime.datetime.now().year 
+#c =  datetime.date.today().year
+
+current_year = datetime.date.today().year
+birth_year = int(input('Ваша дата рождения (укажите год): '))
+your_age =  current_year - birth_year
+print('Вам', your_age, 'лет/год(а).')
+
+
+
+
+
+
